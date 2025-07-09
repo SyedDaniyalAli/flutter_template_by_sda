4c64a3c5011bc56237ca77fd9ae7d638601ec6d8
Syed Daniyal Ali <SyedDaniyalAli2222@gmail.com>
2025-07-09 20:53:23 +0500

    Localize Page Not Found screen
    This commit localizes the text content on the "Page Not Found" screen.
    The following changes were made:
    - Imported `AppLocalizations`.
    - Replaced hardcoded "404 - Page Not Found" in the AppBar title with `AppLocalizations.of(context)!.page_not_found_text`.
    - Replaced hardcoded "Oops! This page doesn't exist." in the body with `AppLocalizations.of(context)!.this_page_does_not_exist`.

--- lib/features/page_not_found/presentation/page_not_found.dart
+++ lib/features/page_not_found/presentation/page_not_found.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
 
+import '../../../core/localizations/l10n/app_localizations.dart';
+
 class NotFoundScreen extends StatelessWidget {
   const NotFoundScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("404 - Page Not Found")),
-      body: const Center(
-        child: Text("Oops! This page doesn't exist."),
+      appBar: AppBar(
+        title: Text(AppLocalizations.of(context)!.page_not_found_text),
+      ),
+      body: Center(
+        child: Text(AppLocalizations.of(context)!.this_page_does_not_exist),
       ),
     );
   }
